6a659719a9c78e388475bd964370cc5ff110172d
dayush3104 <126963799+dayush3104@users.noreply.github.com>
2024-10-26 15:44:42 +0530

    Delete supermarket_automation_market_serve.bat

--- supermarket_automation_market_serve.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend Server...
-start cmd /k "cd D:\Code\Projects\Supermarket Automation System\backend && nodemon index.js"
-
-echo Starting Frontend Server...
-start cmd /k "cd D:\Code\Projects\Supermarket Automation System\frontend && npm start"
-
-exit
